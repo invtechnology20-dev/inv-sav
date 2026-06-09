52a2fd0d52a959ffda6dfd987f082d91b52b5317
invtechnology20-dev <invtechnology20@gmail.com>
2026-06-09 15:48:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-const CACHE_NAME = 'inv-sav-v2';
+const CACHE_NAME = 'inv-sav-v3';
 const ASSETS = [
-  './index.html',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -22,7 +19,6 @@ self.addEventListener('activate', e => {
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
